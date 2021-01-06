7393db4a62ffaf8c36dbc39d007d16f1558d4cdc
thackerdynasty <70179471+thackerdynasty@users.noreply.github.com>
2021-01-05 21:52:03 -0500

    Finish of project
    Play code here: https://repl.it/talk/share/number-that-is-less-than-ones-in-a-list/113278

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ if user_num < 5:
 	print('less than 10')
 	print('less than 50')
 	print('less than 100') 
+elif user_num < 10:
+	print('less than 10')
+	print('less than 50')
+	print('less than 100') 
+elif user_num < 50:
+	print('less than 50')
+	print('less than 100')
+elif user_num < 100:
+	print('less than 100')
+else:
+	print('not less than anything in the list')
